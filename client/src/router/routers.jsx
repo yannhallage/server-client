9e8c6e2c36bd7361d178a7f850b409eb863a932f
yannhallage <contact.devhllg@gmail.com>
2025-05-20 00:32:12 +0000

    je reprend le travail sur server-client

--- client/src/router/routers.jsx
+++ client/src/router/routers.jsx
@@ -11,7 +11,7 @@ const Component_Router = () => {
         <>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Authentification />} />
+                    <Route path="/" element={< Authentification/>} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/create" element={<Signup />} />
                     <Route path="*" element={<Notfound />} />
